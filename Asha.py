6724def5c594563b50ec4f0c34af081ac73c2513
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-20 21:01:29 +0100

    Update Asha.py

--- Asha.py
+++ Asha.py
@@ -1,13 +1,27 @@
-from voice import listen, speak
-from commands import handle_command
+from core.voice import listen, speak
+from core.brain import detect_intent
+from system.files import handle_files
+from memory.memory import handle_memory
+from core.emotions import handle_emotion
 
 def main():
-    speak("ASHA is online. I am listening.")
+    speak("ASHA online. I'm listening.")
 
     while True:
         text = listen()
-        if text:
-            handle_command(text)
+        if not text:
+            continue
 
-if __name__ == "__main__":
-    main()
+        intent = detect_intent(text)
+
+        if intent == "system":
+            handle_files(text)
+
+        elif intent == "memory":
+            handle_memory(text)
+
+        elif intent == "emotion":
+            handle_emotion()
+
+        else:
+            speak("I heard you.")
